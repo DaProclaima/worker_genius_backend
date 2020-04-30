1e49e995878be249b7e9e0de4d99640031930f5b
daproclaima <sn.itpro@gmail.com>
2020-04-30 10:02:27 +0200

    feat: prepared the basic backend features and removed the frontend features. Uses port 3010

--- app/server.js
+++ app/server.js
@@ -2,8 +2,8 @@ const express = require('express')
 const routes = require('./controllers/routes.js')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const exphbs = require('express-handlebars')
-const path = require('path')
+// const exphbs = require('express-handlebars')
+// const path = require('path')
 // const fetch = require('node-fetch')
 
 /**
@@ -13,7 +13,7 @@ const path = require('path')
 class Server {
   constructor () {
     this.app = express()
-    this.port = 3000
+    this.port = 3010
   }
 
   // fecthArticle () {
@@ -75,27 +75,27 @@ class Server {
    * middleware
    */
   middleware () {
-    this.app.set('view engine', '.hbs')
-    this.app.use('/public', express.static(path.join(__dirname, '../assets')))
-    this.app.set('views', path.join(__dirname, '../views'))
-    this.app.engine('.hbs', exphbs({
-      helpers: {
-        dateFormat: require('handlebars-dateformat')
-      },
-      extname: '.hbs',
-      defaultLayout: 'main',
-      layoutsDir: path.join(__dirname, '../views/layouts/'),
-      partialsDir: path.join(__dirname, '../views/partials/')
-    }))
+    // this.app.set('view engine', '.hbs')
+    // this.app.use('/public', express.static(path.join(__dirname, '../assets')))
+    // this.app.set('views', path.join(__dirname, '../views'))
+    // this.app.engine('.hbs', exphbs({
+    //   helpers: {
+    //     dateFormat: require('handlebars-dateformat')
+    //   },
+    //   extname: '.hbs',
+    //   defaultLayout: 'main',
+    //   layoutsDir: path.join(__dirname, '../views/layouts/'),
+    //   partialsDir: path.join(__dirname, '../views/partials/')
+    // }))
 
     this.app.use(bodyParser.urlencoded({ 'extended': true }))
     this.app.use(bodyParser.json())
 
-    this.app.get('/home', async (_, res) => {
-      // const articles = await this.fecthArticle()
-      const title = 'Bienvenue !'
-      res.render('home', { title: title })
-    })
+    // this.app.get('/home', async (_, res) => {
+    //   // const articles = await this.fecthArticle()
+    //   const title = 'Bienvenue !'
+    //   res.render('home', { title: title })
+    // })
 
     this.app.use(bodyParser.urlencoded({ 'extended': true }))
     this.app.use(bodyParser.json())
