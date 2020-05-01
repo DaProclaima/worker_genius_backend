c35326212fff1b8af62f7e191fe632618caf8fc3
daproclaima <sn.itpro@gmail.com>
2020-05-01 15:03:28 +0200

    feat(user/delete.js): creates DELETE method for CRUD of user

--- app/controllers/routes.js
+++ app/controllers/routes.js
@@ -1,23 +1,23 @@
 // Users
 const NewUser = require('./users/new')
 const ShowUser = require('./users/show')
-// const DeleteUser = require('./users/delete')
 const EditUser = require('./users/edit')
+const DeleteUser = require('./users/delete')
 // const ListUser = require('./users/list')
 
 // Certifications
 const NewCertification = require('./certifications/new')
 const ShowCertification = require('./certifications/show')
-const ListCertification = require('./certifications/list')
 const EditCertification = require('./certifications/edit')
 const DeleteCertification = require('./certifications/delete')
+const ListCertification = require('./certifications/list')
 
 module.exports = {
   users: {
     NewUser,
     ShowUser,
-    //   DeleteUser,
-    EditUser
+    EditUser,
+    DeleteUser
   },
   certifications: {
     NewCertification,
--- app/controllers/users/delete.js
+++ app/controllers/users/delete.js
@@ -1,46 +1,44 @@
-// const User = require('../../models/user')
+const User = require('../../models/user')
 // const JWT = require('../../jwt.js')
 // const jwt = new JWT()
-// /**
-//  * Create
-//  * @class
-//  */
-// class Delete {
-//   constructor (app, connect) {
-//     this.app = app
-//     this.UserModel = connect.model('User', User)
-//     this.run()
-//   }
-//   /**
-//    * middleware
-//    */
-//   middleware () {
-//     this.app.delete('/user/delete/:id', jwt.express(), (req, res) => {
-//       try {
-//         const { id } = req.params
-//         this.UserModel.findByIdAndDelete(id).then(user => {
-//           res.status(200).json(user || {})
-//         }).catch(err => {
-//           res.status(500).json({
-//             'code': 500,
-//             'message': err
-//           })
-//         })
-//       } catch (err) {
-//         res.status(500).json({
-//           'code': 500,
-//           'message': err
-//         })
-//       }
-//     })
-//   }
 
-//   /**
-//    * run
-//    */
-//   run () {
-//     this.middleware()
-//   }
-// }
+/**
+ * Create
+ * @class
+ */
+class Delete {
+  constructor (app, connect, apiPrefix) {
+    this.app = app
+    this.apiPrefix = apiPrefix
+    this.UserModel = connect.model('User', User)
+    this.run()
+  }
+  /**
+   * middleware
+   */
+  middleware () {
+    this.app.delete(`${this.apiPrefix}/user/delete/:slug`, (req, res) => {
+      try {
+        const { slug } = req.params
+        this.UserModel.findOneAndDelete({slug: slug})
+          .then(model => {
+            res.status(200).json(model || {})
+          })
+      } catch (err) {
+        res.status(500).json({
+          'code': 500,
+          'message': err
+        })
+      }
+    })
+  }
 
-// module.exports = Delete
+  /**
+   * run
+   */
+  run () {
+    this.middleware()
+  }
+}
+
+module.exports = Delete
